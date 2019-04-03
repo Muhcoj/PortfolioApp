3dd373109319c854fb813f49e879fc48d4a6262a
Muhcoj <jochum.de.bueger@gmail.com>
2019-04-03 21:34:27 +0200

    Refactored alerts into helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -56,6 +56,18 @@ module ApplicationHelper
 	def active? path
 		"active" if current_page? path
 	end
+
+	def alerts
+		alert = (flash[:error] || flash[:alert] || flash[:notice])
+
+		if alert
+			alert_generator alert 
+		end
+	end	
+
+	def alert_generator msg
+		js add_gritter(msg, title: "Jochum de Bueger Portfolio", sticky: false, time: 2000)
+	end
 end
 
 
